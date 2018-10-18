d0982062f2980be2bd6b5a6444365130a4a47293
Inga <mamury86@gmail.com>
2018-10-18 11:12:30 +0900

    first comment

---
+++ js.js
@@ -0,0 +1,25 @@
+let money = +prompt('Your budget?');
+let name = prompt('The name of your shop?');
+
+let mainList = {
+    budget: money,
+    nameShop: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+
+};
+
+
+mainList.shopGoods[0] = prompt('What type of products will sell?');
+mainList.shopGoods[2] = prompt('What type of products will sell?');
+mainList.shopGoods[1] = prompt('What type of products will sell?');
+
+alert(mainList.budget / 30);
+
+console.log(mainList);
+
+
+let num = 33721;
+let str =
+console.log();
